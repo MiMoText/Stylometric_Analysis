af76b4a72cdac3e872616bfd827005ae7b70c49c
JoKons <johanna.konstanciak@gmx.de>
2023-10-06 12:16:53 +0200

    moved script to folder

--- narrative_form_analysis/stylo_narrativeForm.R
+++ narrative_form_analysis/stylo_narrativeForm.R
@@ -6,7 +6,7 @@ library(tools)
 
 # same with corpus more than 3 authors in narrative Form
 
-set.3 <- load.corpus.and.parse(corpus.dir="corpora/corpus_with_narr_form", corpus.lang="French")
+set.3 <- load.corpus.and.parse(corpus.dir="../corpora/corpus_with_narr_form", corpus.lang="French")
 freq.list.3 <- make.frequency.list(set.3, head=4000)
 frq.table.3 <- make.table.of.frequencies(corpus = set.3, features=freq.list.3)
 
@@ -14,6 +14,7 @@ frq.table.3 <- make.table.of.frequencies(corpus = set.3, features=freq.list.3)
 # Dendrogram
 result <- stylo(gui=FALSE, frequencies = frq.table.3, mfw.min=3500, mfw.max=3500, corpus.lang ="French", distance.measure="wurzburg",
                 display.on.screen=FALSE, write.png.file = TRUE, plot.custom.height = 9, plot.font.size=6)
+                
 
 stylo(gui=FALSE, frequencies = frq.table.3, mfw.min=2000, mfw.max=4000, corpus.lang ="French", distance.measure="wurzburg",
       display.on.screen=FALSE, analysis.type="BCT", write.png.file = TRUE, plot.custom.height = 6, plot.custom.width=6,  plot.font.size=6)
