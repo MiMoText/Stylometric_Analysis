5d19ec978b991a9b74a0af51889fa7f28c725fdd
JoKons <johanna.konstanciak@gmx.de>
2023-10-06 14:23:06 +0200

    new commits

--- corpus_vs_corpus_more_than_3.R
+++ corpus_vs_corpus_more_than_3.R
@@ -26,8 +26,12 @@ frq.table.3 <- make.table.of.frequencies(corpus = set.3, features=freq.list.3)
 
 
 # Dendrogram
-result <- stylo(gui=FALSE, frequencies = frq.table.3, mfw.min=3500, mfw.max=3500, corpus.lang ="French", distance.measure="wurzburg",
-                display.on.screen=FALSE, write.png.file = TRUE, plot.custom.height = 15, plot.font.size=6)
+result_dendo <- stylo(gui=FALSE, frequencies = frq.table.3, mfw.min=3500, mfw.max=3500, corpus.lang ="French", distance.measure="wurzburg",
+                display.on.screen=FALSE, write.png.file = TRUE, plot.custom.height = 9, plot.font.size=6)
 
-stylo(gui=FALSE, frequencies = frq.table.3, mfw.min=100, mfw.max=2000, corpus.lang ="French", distance.measure="wurzburg",
-      display.on.screen=FALSE, analysis.type="BCT", write.png.file = TRUE, plot.custom.height = 9, plot.font.size=6)
+# BootStrap Consensus Tree
+result_bct <- stylo(gui=FALSE, frequencies = frq.table.3, mfw.min=2000, mfw.max=4000, corpus.lang ="French", distance.measure="wurzburg",
+      display.on.screen=FALSE, analysis.type="BCT", write.png.file = TRUE, plot.custom.height = 8, plot.font.size=6)
+
+
+write.csv(result_dendo$distance.table, sprintf("distance_table_3500MFW_wurzburg_new.csv"), row.names = TRUE)
