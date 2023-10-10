641181eaa15c218e0690f64131c8389eaacc6b8b
JoKons <johanna.konstanciak@gmx.de>
2023-10-10 11:35:02 +0200

    added new script

---
+++ get_distances.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+with open("distance_table_3500MFW_wurzburg_new2.csv", "r", encoding="utf8") as infile:
+    dist_table = pd.read_csv(infile, sep=",")
+
+
+dist_table = dist_table.rename(columns={"Unnamed: 0" : "text_dist"})
+print(dist_table.head())
+anonym = dist_table[["text_dist", "Anonym_Enfant"]]
+print(anonym)
+
+print(anonym.min(skipna=True))
+print(anonym.nsmallest(10, "Anonym_Enfant"))
+
+nerciat = dist_table[["text_dist", "Nerciat_Felicia"]]
+print(nerciat.nsmallest(10, "Nerciat_Felicia"))
