28390c9d560a18b8271020df74434a118b454e16
JoKons <johanna.konstanciak@gmx.de>
2025-05-16 12:35:04 +0200

    updated script and created dist tables

--- Analysis_Paper/stylometric_analysis.R
+++ Analysis_Paper/stylometric_analysis.R
@@ -34,7 +34,8 @@ min.mfws.bct = 2000 # mfw.min for Bootstrap Consensus Tree
 max.mfws.bct = 3500 # mfw.max for Bootstrap Consensus Tree
 
 # path and filename for distance tables
-output.filename = sprintf("results/distance_table_%sMFW_wurzburg.csv", mfws)
+#output.filename = sprintf("results/distance_table_%s_MFW_wurzburg", mfws)
+
 
 # path where plots will be saved
 path.plots ="results/"
@@ -57,32 +58,25 @@ result_bct <- stylo(gui=FALSE, frequencies = frq.table, mfw.min=min.mfws.bct, mf
                     display.on.screen=FALSE, analysis.type="BCT", write.png.file = TRUE, plot.custom.height = 8, plot.font.size=6, path = path.plots)
 
 
+result_dendo$distance.table
+
 # Save distance table
-write.csv(result_dendo$distance.table, sprintf("%s.csv", output.filename), row.names = TRUE)
+write.csv(result_dendo$distance.table, sprintf("results/dist_tables/distance_table_%s_MFW_wurzburg.csv", mfws), row.names = TRUE)
+write.csv(result_bct$distance.table, sprintf("results/dist_tables/distance-table-bct-2000-3500-mfw.csv"), row.names = TRUE)
 
 
 # Step 2: General Imposters Method
 ## taking 2000 MFWs
-imposters2000 = imposters(reference.set = frq.table.all[-c(1), 1:2000], test = frq.table.all[1,1:2000], distance="wurzburg")
+imposters2000 = imposters(reference.set = frq.table[-c(1), 1:2000], test = frq.table[1,1:2000], distance="wurzburg")
 imposters2000
 
-df = data.frame(as.list(imposters2000))
-
-counter <- 0
-
-while (counter <= 10 ) {
-  new_imp = imposters(reference.set = frq.table.all[-c(1), 1:2000], test = frq.table.all[1,1:2000], distance="wurzburg")
-  df <- rbind(df, new_imp)
-  counter <- counter + 1
-  
-} 
 
 #=> Mirabeau 	 0.29
 #Nerciat 	 0.06
 #PigaultLebrun 	 0.78
 #Retif 	 0.65
 
-optim_imposters2000_w = imposters.optimize(frq.table.all[, 1:2000], distance="wurzburg")
+optim_imposters2000_w = imposters.optimize(frq.table[, 1:2000], distance="wurzburg")
 optim_imposters2000_w
 
 # results [1] 0.29 0.68
@@ -93,9 +87,9 @@ optim_imposters2000_w
 
 
 #### MFW = 500
-grep("Anonym_Enfant", rownames(frq.table.all))
+grep("Anonym_Enfant", rownames(frq.table))
 
-imposters = imposters(reference.set = frq.table.all[-c(1), 1:500], test = frq.table.all[1,1:500], distance="wurzburg")
+imposters = imposters(reference.set = frq.table[-c(1), 1:500], test = frq.table[1,1:500], distance="wurzburg")
 imposters
 ## NEU
 #Mirabeau 	 0.21
@@ -106,11 +100,11 @@ imposters
 # optimizing decision scores:
 # based on alogrithm by Kesetmont et al. based on c@1 measure
 # only authors with >1 samples are used (Anonym is deleted)
-optim_imposters = imposters.optimize(frq.table.all[, 1:500])
+optim_imposters = imposters.optimize(frq.table[, 1:500])
 optim_imposters
 # output = parameter p1 and p2
 
-optim_imposters_w = imposters.optimize(frq.table.all[, 1:500], distance="wurzburg")
+optim_imposters_w = imposters.optimize(frq.table[, 1:500], distance="wurzburg")
 optim_imposters_w
 
 # => results [1] 0.40 0.58
@@ -119,18 +113,18 @@ optim_imposters_w
 
 
 # Test if Enfant was written by PL:
-text_to_be_tested = frq.table.all[1,] # Anonym_Enfant
+text_to_be_tested = frq.table[1,] # Anonym_Enfant
 ## Check PL
-grep("PigaultLebrun_Adele", rownames(frq.table.all))
-my_candidate = frq.table.all[71:74,]
-my_imposters = frq.table.all[-c(1, 71:74),]
+grep("PigaultLebrun_Adele", rownames(frq.table))
+my_candidate = frq.table[71:74,]
+my_imposters = frq.table[-c(1, 71:74),]
 imp_refs = imposters(reference.set = my_imposters, test = text_to_be_tested, candidate.set = my_candidate, features=0.5)
 imp_refs
 
 ## Check Mirabeau
-grep("Mirabeau_Conversion", rownames(frq.table.all))
-mirabeau = frq.table.all[65:66,]
-mirabeau_imps = frq.table.all[-c(1, 65:66),]
+grep("Mirabeau_Conversion", rownames(frq.table))
+mirabeau = frq.table[65:66,]
+mirabeau_imps = frq.table[-c(1, 65:66),]
 mirabeau_refs = imposters(reference.set = mirabeau_imps, test = text_to_be_tested, candidate.set = mirabeau, features=0.5)
 mirabeau_refs
 
